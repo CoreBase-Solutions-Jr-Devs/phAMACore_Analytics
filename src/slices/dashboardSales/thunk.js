329319af8bc7b782bb415553be6f32d5c365193d
Essy116 <githinjie40@gmail.com>
2026-05-15 15:49:24 +0300

    fix(powerbi): correct sales transactions thunk implementation

--- src/slices/dashboardSales/thunk.js
+++ src/slices/dashboardSales/thunk.js
@@ -1,1 +1,20 @@
-c
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+import {
+  getSalesTransactions as getSalesTransactionsApi,
+} from "../../helpers/fakebackend_helper";
+
+// GET SALES
+export const getSalesTransactions = createAsyncThunk(
+  "powerbi/getSalesTransactions",
+  async (params) => {
+    try {
+      const response = await getSalesTransactionsApi(params);
+      return response;
+    } catch (error) {
+      toast.error("Failed to fetch sales transactions", { autoClose: 3000 });
+      return error;
+    }
+  }
+);
