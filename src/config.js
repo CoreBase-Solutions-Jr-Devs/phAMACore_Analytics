4068b035b6019da31217308adfe4a3ab3b273d89
Essy116 <githinjie40@gmail.com>
2026-07-27 08:57:14 +0300

    fix(config): update API endpoints to match environment configuration

--- src/config.js
+++ src/config.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   api: {
     API_URL: "https://api-node.themesbrand.website",
-    AUTH_API_URL: "https://www.phamacoretraining.co.ke:81",
-    POWERBI_API_URL: "https://www.phamacoredev.co.ke:81"
+    AUTH_API_URL: "https://phamacoredev.co.ke:89",
+    POWERBI_API_URL: "https://www.phamacoredev.co.ke:81",
+    // LOGOUT_API_URL: "https://phamacoredev.co.ke:81",
   }
 };
