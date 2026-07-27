f996ab19f9e3ebd54b91949128c20b5902dd009c
Essy116 <githinjie40@gmail.com>
2026-07-27 08:55:05 +0300

    refactor(auth): remove login background image and top container padding

--- src/pages/AuthenticationInner/ParticlesAuth.js
+++ src/pages/AuthenticationInner/ParticlesAuth.js
@@ -4,7 +4,7 @@ import withRouter from '../../Components/Common/withRouter';
 const ParticlesAuth = ({ children }) => {
     return (
         <React.Fragment>
-            <div className="auth-page-wrapper pt-5">
+            <div className="auth-page-wrapper ">
                 <div className="auth-one-bg-position auth-one-bg" id="auth-particles">
 
                     <div className="bg-overlay"></div>
