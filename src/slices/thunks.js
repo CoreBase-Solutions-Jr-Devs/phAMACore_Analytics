819db8a3c392007a9d84047c1f604f31e40b3159
mikewanjero <mikewanj@gmail.com>
2026-07-28 09:07:23 +0300

    {FEAT} Redux implementation - adding reducers to root reducer

--- src/slices/thunks.js
+++ src/slices/thunks.js
@@ -6,6 +6,8 @@ export * from "./auth/login/thunk";
 export * from "./auth/register/thunk";
 export * from "./auth/forgetpwd/thunk";
 export * from "./auth/profile/thunk";
+export * from "./auth/changepassword/thunk";
+export * from "./auth/resetpassword/thunk";
 
 //Calendar
 export * from "./calendar/thunk";
